8c95fc52f599c10d861960cc645a7246b884964a
BhaskarAcharjee <bhaskaracharjee2002@gmail.com>
2025-08-04 02:23:16 +0530

    Add Features, Screenshots Section & Update links, images

--- assets/js/main.js
+++ assets/js/main.js
@@ -116,3 +116,53 @@
   });
   wow.init();
 })();
+
+//========== Carousel Functionality ==========
+// This code handles the carousel functionality, allowing users to click on items to change the active item
+const state = {};
+const carouselList = document.querySelector(".carousel__list");
+const carouselItems = document.querySelectorAll(".carousel__item");
+const elems = Array.from(carouselItems);
+
+carouselList.addEventListener("click", function (event) {
+  const clickedElem = event.target.closest(".carousel__item");
+  if (!clickedElem || clickedElem.dataset.pos == "0") return;
+
+  update(clickedElem);
+});
+
+const update = function (newActive) {
+  const newActivePos = newActive.dataset.pos;
+
+  const current = elems.find((elem) => elem.dataset.pos == 0);
+  const prev = elems.find((elem) => elem.dataset.pos == -1);
+  const next = elems.find((elem) => elem.dataset.pos == 1);
+  const first = elems.find((elem) => elem.dataset.pos == -2);
+  const last = elems.find((elem) => elem.dataset.pos == 2);
+
+  current.classList.remove("carousel__item_active");
+
+  [current, prev, next, first, last].forEach((item) => {
+    var itemPos = item.dataset.pos;
+
+    item.dataset.pos = getPos(itemPos, newActivePos);
+  });
+};
+
+const getPos = function (current, active) {
+  const diff = current - active;
+
+  if (Math.abs(current - active) > 2) {
+    return -current;
+  }
+
+  return diff;
+};
+
+setInterval(() => {
+  const current = elems.find((elem) => elem.dataset.pos == 0);
+  const nextIndex = (elems.indexOf(current) + 1) % elems.length;
+  const next = elems[nextIndex];
+
+  update(next);
+}, 3000); // change every 3 seconds
